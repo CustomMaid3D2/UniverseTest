95fdc0b4b00eb723ff3a40046d29146c70c18d43
lilly <lilly1987@naver.com>
2022-05-27 14:13:01 +0900

    초기 커밋.

---
+++ UniverseTest/UniverseTest.cs
@@ -0,0 +1,82 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UniverseLib;
+using UniverseLib.Config;
+using UniverseLib.UI;
+
+namespace UniverseTest
+{
+    public class MyAttribute
+    {
+        public const string PLAGIN_NAME = "UniverseTest";
+        public const string PLAGIN_VERSION = "22.05.27";
+        public const string PLAGIN_FULL_NAME = "COM3D2.UniverseTest.Plugin";
+    }
+
+    [BepInPlugin(MyAttribute.PLAGIN_FULL_NAME, MyAttribute.PLAGIN_NAME, MyAttribute.PLAGIN_VERSION)]// 버전 규칙 잇음. 반드시 2~4개의 숫자구성으로 해야함. 미준수시 못읽어들임
+    public class UniverseTest : BaseUnityPlugin
+    {
+        public static ManualLogSource log;
+        public static ConfigFile config;
+
+        public static UIBase myUIBase { get; private set; }
+        public static UniverseTestPanel myPanel { get; private set; }
+
+        public void Awake()
+        {
+            log = Logger;
+            config = Config;
+            log.LogMessage("Awake");
+
+            UniverseLibConfig universeLibConfig = new UniverseLibConfig
+            {
+                Force_Unlock_Mouse = new bool?(false),
+                Disable_EventSystem_Override = new bool?(true),
+                Allow_UI_Selection_Outside_UIBase = new bool?(true)
+            };
+            Universe.Init(0, UniverseInit, LogHandler, universeLibConfig);
+        }
+
+        private void UniverseInit()
+        {
+            log.LogMessage("UniverseInit st");
+
+            myUIBase = UniversalUI.RegisterUI(MyAttribute.PLAGIN_NAME, UiUpdate);
+            myUIBase.Enabled = true;
+
+            myPanel = new UniverseTestPanel(myUIBase, config, log);
+            myPanel.Enabled = true;
+
+            log.LogMessage("UniverseInit ed");
+        }
+
+        private void UiUpdate()
+        {
+
+        }
+
+        private static void LogHandler(string log, LogType logType)
+        {
+            switch (logType)
+            {
+                case LogType.Error:
+                case LogType.Exception:
+                    UniverseTest.log.LogError(log);
+                    break;
+                case LogType.Assert:
+                case LogType.Warning:
+                    UniverseTest.log.LogWarning(log);
+                    break;
+                case LogType.Log:
+                    UniverseTest.log.LogMessage(log);
+                    break;
+            }
+        }
+    }
+}
---
+++ UniverseTest/UniverseTestPanel.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx.Configuration;
+using BepInEx.Logging;
+using UnityEngine;
+using UnityEngine.UI;
+using UniverseLib.UI;
+using UniverseLib.UI.Panels;
+
+namespace UniverseTest
+{
+    public class UniverseTestPanel : PanelBase
+    {
+        public static ManualLogSource log;
+        public static ConfigFile config;
+
+        public UniverseTestPanel(UIBase owner, BepInEx.Configuration.ConfigFile Config, ManualLogSource Logger) : base(owner)
+        {
+            log = Logger;
+            config = Config;
+            log.LogMessage("UniverseTestPanel");
+        }
+
+        public override string Name => MyAttribute.PLAGIN_NAME;
+        public override int MinWidth => 200;
+        public override int MinHeight => 600;
+        public override Vector2 DefaultAnchorMin => new Vector2(0.25f, 0.25f);
+        public override Vector2 DefaultAnchorMax => new Vector2(0.75f, 0.75f);
+        public override bool CanDragAndResize => true;
+
+        protected override void ConstructPanelContent()
+        {
+            log.LogMessage("ConstructPanelContent");
+            Text myText = UIFactory.CreateLabel(ContentRoot, "myText", "Hello world");
+            UIFactory.SetLayoutElement(myText.gameObject, minWidth: 200, minHeight: 25);
+        }
+    }
+}
